b8911d3bfb2c3bd09e461692ac29f28aaad21b97
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2026-01-20 18:07:44 -0500

    Lab 11 finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Container, Grid } from '@mui/material'
 import Header from './components/Header'
 import PokemonList from './pages/PokemonList'
 import PokemonForm from './pages/PokemonForm'
+import LoginPage from './pages/LoginPage'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -9,12 +10,13 @@ function App() {
 
   return (
     <>
-      <Header />
       <Container>
         <BrowserRouter>
+          <Header />
           <Routes>
             <Route path='/' element={<PokemonList />} />
             <Route path='/add-pokemon' element={<PokemonForm />} />
+            <Route path='/login' element={<LoginPage />} />
           </Routes>
         </BrowserRouter>
       </Container>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,48 @@
 import { AppBar, Button, Container, Toolbar } from "@mui/material";
 import pokedexLogo from "../assets/pokedex-logo.png";
-
-import './Header.css';
+import { logout } from "../services/authService";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const isLoggedIn = localStorage.getItem("access_token") !== null;
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    alert("Sesión cerrada exitosamente");
+    navigate("/");
+  };
+
   return (
     <header className="pokedex-navbar">
-      <Container>
-        <AppBar position="static">
-          <Toolbar>
-            <div className="image-container">
-              <img src={pokedexLogo} alt="Pokédex Logo" height={100} />
-            </div>
-          </Toolbar>
-          <Toolbar>
-            <Button color="inherit" href="/">Inicio</Button>
-            <Button color="inherit" href="/add-pokemon">Agregar Pokémon</Button>
-          </Toolbar>
-        </AppBar>
-      </Container>
+      <AppBar position="static">
+        <Toolbar>
+          <div className="image-container">
+            <img src={pokedexLogo} alt="Pokédex Logo" height={100} />
+          </div>
+        </Toolbar>
+        <Toolbar>
+          <Button color="inherit" href="/">
+            Inicio
+          </Button>
+          {isLoggedIn && (
+            <>
+              <Button color="inherit" href="/add-pokemon">
+                Agregar Pokémon
+              </Button>
+              <Button color="inherit" onClick={handleLogout}>
+                Cerrar sesión
+              </Button>
+            </>
+          )}
+          {!isLoggedIn && (
+            <Button color="inherit" href="/login">
+              Iniciar sesión
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
     </header>
   );
 }
